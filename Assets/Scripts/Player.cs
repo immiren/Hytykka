3b1f8b862af14b3fcbb4b93b0dfe0cfd29ab14d2
immiren <immi.renko@gmail.com>
2025-01-25 15:54:09 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,7 +16,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             bc.Fire();
         }
